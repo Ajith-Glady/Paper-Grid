645b636a74d244d5175f0dd5eb41d65237918688
Ajith Glady <112477142+Ajith-Glady@users.noreply.github.com>
2023-10-25 16:06:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+function mOver(obj){
+    obj.src='images/Paper grid-1-1.jpeg'
+}
+
+function mOut(obj){
+    obj.src='images/Papergrid-1.jpg'
+} 
+
+
+function mOver3(obj){
+    obj.src='images/Paper grid-3-1.jpg'
+}
+
+function mOut3(obj){
+    obj.src='images/Paper grid-3.jpg'
+}
+
+
+function mOver4(obj){
+    obj.src='images/Paper grid-4-1.jpg'
+}
+
+function mOut4(obj){
+    obj.src='images/Paper grid-4.jpg'
+}
+
+
+function mOver5(obj){
+    obj.src='images/Paper grid-5-1.jpg'
+}
+
+function mOut5(obj){
+    obj.src='images/Paper grid-5.jpg'
+}
+
+
+function mOver6(obj){
+    obj.src='images/Paper grid-6-1.jpg'
+}
+
+function mOut6(obj){
+    obj.src='images/Paper grid-6.jpg'
+}
+
+
+function mOver7(obj){
+    obj.src='images/Paper grid-7-1.jpg'
+}
+
+function mOut7(obj){
+    obj.src='images/Paper grid-7.jpg'
+}
+
+
+
+function mOver8(obj){
+    obj.src='images/Paper grid-8-1.jpg'
+}
+
+function mOut8(obj){
+    obj.src='images/Paper grid-8.jpg'
+}
+
+
+
+function mOver9(obj){
+    obj.src='images/Paper grid-9-1.jpg'
+}
+
+function mOut9(obj){
+    obj.src='images/Paper grid-9.jpg'
+}
